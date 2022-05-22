9438a4f50bfc2579e5ca89d8fb5073d039bdb8e7
qingl <412753863@qq.com>
2022-05-22 21:41:01 +0800

    新的登录及主页面路由

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,12 @@ import router from "@/router";
 import ElementUI from "element-ui";
 import "@/styles/element_ui.scss";
 import BaiduMap from "vue-baidu-map";
-import { MyAxios } from "@/typings/MyAxios";
 
 Vue.config.productionTip = false;
 
 Vue.use(ElementUI); // element ui
 Vue.use(BaiduMap, { ak: "HpA4MtzOdUZ9Ywnum4HHwtnbwcIrn0Hc" }); // baidu map
 Vue.config.ignoredElements = [/^ion-/]; //ion-icon
-MyAxios.init(); // axios
 
 new Vue({
 	router,
--- src/typings/Tools.ts
+++ src/typings/Tools.ts
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import router from "@/router";
+import { Vue } from "vue-property-decorator";
 
-export class Tools {
+export class Tools extends Vue {
 	// 添加参数到 vue router
 	public static addRouterParam(key: string, value: string) {
 		const route = router.currentRoute;
@@ -36,5 +37,61 @@ export class Tools {
 		if (path != url || JSON.stringify(query) != JSON.stringify(params)) {
 			router.push({ path: url, query: params });
 		}
+	}
+
+	// 单例模式
+	private static _instance: Tools | undefined;
+
+	static get instance(): Tools {
+		if (this._instance == undefined) {
+			this._instance = new Tools();
+		}
+		return this._instance;
+	}
+
+	// 成功消息提示
+	public static success(msg: string) {
+		this.instance.$notify({
+			type: "success",
+			title: "成功",
+			message: msg,
+		});
+	}
+
+	// 错误消息提示
+	public static error(msg: string) {
+		this.instance.$notify.error({
+			title: "错误",
+			message: msg,
+		});
+	}
+
+	public static logout() {
+		if (Tools.status != "unlogged") {
+			Tools.status = "unlogged";
+			Tools.success("退出登录");
+		}
+	}
+
+	constructor() {
+		super();
+	}
+
+	public static status: string; // page status
+
+	private static _token: string | undefined;
+	public static get token(): string {
+		if (this._token == undefined) {
+			const authentication = localStorage.getItem("authentication");
+			this._token = authentication == undefined ? "" : authentication;
+		}
+		return this._token;
+	}
+	public static set token(new_value: string) {
+		const old_value = this._token;
+		this._token = new_value;
+		if (new_value == "" && old_value != "")
+			localStorage.removeItem("authentication");
+		localStorage.setItem("authentication", new_value);
 	}
 }
